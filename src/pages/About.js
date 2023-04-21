0757514c7d76a63cae36741733381ac5e823094d
Ivan <ivanwpang@gmail.com>
2023-04-21 10:50:07 -0700

    Updated About paragraph

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,10 +12,7 @@ const About = () => {
               HNOI Senior Design Project: Diesel Engine Performance Tool
             </h3>
             <p style={{ textAlign: "center" }}>
-              The Diesel Engine Performance Tool is a web tool that displays both
-              technical and non technical analytics for clients. These analytics display
-              a variety of stats such gas saved, hydrogen level, Co2 reduction, diesel
-              particulate reduction, etc.
+              The Diesel Engine Performance Tool is a software application designed to provide accurate and detailed analysis of the performance of diesel engines. The tool allows users to monitor and measure various engine parameters such as fuel consumption, emissions, temperature, pressure, and other key performance indicators. By collecting and analyzing real-time data, the tool helps users identify potential issues or areas for improvement in the engine's operation, leading to increased efficiency, reduced emissions, and improved overall performance. The tool can be used by engine manufacturers, automotive engineers, mechanics, and other professionals who are involved in the design, testing, and maintenance of diesel engines. Its user-friendly interface and comprehensive data analysis capabilities make it an essential tool for anyone looking to optimize diesel engine performance.
             </p>
             <p className="card" style={{ textAlign: "center", backgroundColor: "#252526" }}>
                 <img style={{ }} src={Truck}/>
