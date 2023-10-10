b4a8d79a242d7ccd17d5b1234a7ad23f104fd646
CameronSettles <camsettles662@gmail.com>
2023-10-10 00:20:20 -0700

    grid fix

--- src/pages/Dashoard.js
+++ src/pages/Dashoard.js
@@ -10,24 +10,33 @@ const Dashboard = () => {
 
 
   return (
-    <div>
-      <Grid container spacing={1} style={{ backgroundColor: "#f0f0f0" }}>
-        <Grid xs={6} style={gridStyle}>
-          <LiveLineChart
-            botAxisLabel="Time"
-            leftAxisLabel="Particulate Matter"
-          />
-        </Grid>
+    <Grid container spacing={2}>
 
-        <Grid xs={6} style={gridStyle}>
-          <LiveLineChart botAxisLabel="Time" leftAxisLabel="NOx" />
-        </Grid>
-
-        <Grid xs={12} style={gridStyle}>
-          <LiveLineChart botAxisLabel="Time" leftAxisLabel="CO" />
-        </Grid>
+      <Grid xs={6} style={{ height: 400 }}>
+        <LiveLineChart
+          color="hsl(304, 70%, 50%)"
+          botAxisLabel="Time"
+          leftAxisLabel="Particulate Matter"
+        />
       </Grid>
-    </div>
+
+      <Grid xs={6} style={{ height: 400 }}>
+        <LiveLineChart
+          color="hsl(239, 70%, 50%)"
+          botAxisLabel="Time"
+          leftAxisLabel="NOx"
+        />
+      </Grid>
+
+      <Grid xs={12} style={{ height: 400 }}>
+        <LiveLineChart
+          color="hsl(142, 70%, 50%)"
+          botAxisLabel="Time"
+          leftAxisLabel="CO"
+        />
+      </Grid>
+      
+    </Grid>
   );
 };
 
